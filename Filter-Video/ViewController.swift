d21a715bce0d68ed8184b2fa8d56f89902f190e2
Noel-96 <noelorion69@gmail.com>
2020-03-02 11:15:39 +0100

    Initial Commit

---
+++ Filter-Video/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Filter-Video
+//
+//  Created by Noel Obaseki on 02/03/2020.
+//  Copyright © 2020 NoelObaseki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Filter-VideoTests/Filter_VideoTests.swift
@@ -0,0 +1,34 @@
+//
+//  Filter_VideoTests.swift
+//  Filter-VideoTests
+//
+//  Created by Noel Obaseki on 02/03/2020.
+//  Copyright © 2020 NoelObaseki. All rights reserved.
+//
+
+import XCTest
+@testable import Filter_Video
+
+class Filter_VideoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
